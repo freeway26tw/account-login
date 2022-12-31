86f3d3fe2138310b539899eaac7417b8a67714f1
freeway26tw <freeway26tw@gmail.com>
2022-12-31 22:19:35 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+const routes = require('./routes')
 const app = express()
 mongoose.connect(process.env.MONGODB_URI)
 
@@ -19,8 +21,13 @@ db.once('open', () => {
 })
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
+
+app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main" }));
+app.set('view engine', 'hbs')
+
+app.use(routes)
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
---
+++ routes/modules/home.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+
+const Login = require('../../models/login')
+
+router.get('/', (req, res) => {
+  L
+})
