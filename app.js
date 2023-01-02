e1d838662c722180ae392a9f1644d436448debc4
freeway26tw <freeway26tw@gmail.com>
2023-01-02 15:16:34 +0800

    feat: finish login function

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -9,6 +11,7 @@ const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main" }));
 app.set('view engine', 'hbs')
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,8 +2,50 @@ const express = require('express')
 const router = express.Router()
 const Login = require('../../models/login')
 
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman'
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday'
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram'
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!'
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password'
+  }
+]
+
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+router.post('/login', (req, res) => {
+  const { email, password } = req.body
+  const check = users.find((user) => {
+    return user.email === email && user.password === password
+  })
+  if (check) {
+    const firstName = check.firstName
+    res.render('success', { firstName })
+  } else {
+    res.render('index', { error: 'Username 或Password 錯誤' })
+  }
+
 })
 
 module.exports = router
