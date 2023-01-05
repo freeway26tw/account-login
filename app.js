4bb8a352c1ac2a708c7556610d16029fee2b0801
freeway26tw <freeway26tw@gmail.com>
2023-01-05 16:37:28 +0800

    feat: automate login by session

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-
+const session = require('express-session')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -12,6 +12,12 @@ require('./config/mongoose')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'mySecret',
+  name: 'user',
+  saveUninitialized: false,
+  resave: true,
+}))
 
 app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main" }));
 app.set('view engine', 'hbs')
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,46 +2,67 @@ const express = require('express')
 const router = express.Router()
 const Login = require('../../models/login')
 
-const users = [
-  {
-    firstName: 'Tony',
-    email: 'tony@stark.com',
-    password: 'iamironman'
-  },
-  {
-    firstName: 'Steve',
-    email: 'captain@hotmail.com',
-    password: 'icandothisallday'
-  },
-  {
-    firstName: 'Peter',
-    email: 'peter@parker.com',
-    password: 'enajyram'
-  },
-  {
-    firstName: 'Natasha',
-    email: 'natasha@gamil.com',
-    password: '*parol#@$!'
-  },
-  {
-    firstName: 'Nick',
-    email: 'nick@shield.com',
-    password: 'password'
+function auth(req, res, next) {
+  if (req.session.user) {
+    console.log('authenticated')
+    next()
+  } else {
+    console.log('not authenticated')
+    return res.redirect('/')
   }
-]
+}
 
 router.get('/', (req, res) => {
+  console.log(req.session)
+  if (req.session.user) {
+    return res.redirect('/success')
+  }
   res.render('index')
 })
 
+router.get('/success', (req, res) => {
+  const firstName = req.session.user
+  res.render('success', { firstName })
+})
+
 router.post('/login', (req, res) => {
+  const users = [
+    {
+      firstName: 'Tony',
+      email: 'tony@stark.com',
+      password: 'iamironman'
+    },
+    {
+      firstName: 'Steve',
+      email: 'captain@hotmail.com',
+      password: 'icandothisallday'
+    },
+    {
+      firstName: 'Peter',
+      email: 'peter@parker.com',
+      password: 'enajyram'
+    },
+    {
+      firstName: 'Natasha',
+      email: 'natasha@gamil.com',
+      password: '*parol#@$!'
+    },
+    {
+      firstName: 'Nick',
+      email: 'nick@shield.com',
+      password: 'password'
+    }
+  ]
+
   const { email, password } = req.body
   const check = users.find((user) => {
     return user.email === email && user.password === password
   })
+
   if (check) {
     const firstName = check.firstName
-    res.render('success', { firstName })
+    req.session.user = check.firstName
+    res.redirect('/success')
   } else {
     res.render('index', { error: 'Username 或Password 錯誤' })
   }
